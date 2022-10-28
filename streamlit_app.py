4a9f1470938ed9892d1011d57a60a334c3346cc8
projectlocale <afgcas@gmail.com>
2022-10-28 16:50:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('This is the header')
+streamlit.text('This is text.')
+streamlit.text('This is also text.\n')
+streamlit.text('Hey, more text.')
