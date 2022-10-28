00d0c06d045d7c3171394ed1d57d4fd792e67016
projectlocale <afgcas@gmail.com>
2022-10-28 16:59:38 -0400

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('This is the header')
 streamlit.text('This is text.')
-streamlit.text('This is also text.\n')
+streamlit.text('This is also text.\n 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Hey, more text.')
