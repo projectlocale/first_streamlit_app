a7874cb50a3867d024a4759f476f971189ee68e3
projectlocale <afgcas@gmail.com>
2022-10-28 17:18:34 -0400

    Update streamlit_app.py
    Pick interaction

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,8 @@ streamlit.text('This is text.')
 streamlit.text('This is also text.\n 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Hey, more text.')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list))
+
 streamlit.dataframe(my_fruit_list)
