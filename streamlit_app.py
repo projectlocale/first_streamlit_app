57732e1f04a247c8fe1a1d462d022cf485c86651
projectlocale <afgcas@gmail.com>
2022-10-28 17:07:04 -0400

    Update streamlit_app.py
    Using pandas library.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('This is the header')
 streamlit.text('This is text.')
 streamlit.text('This is also text.\n 🥣 🥗 🐔 🥑🍞')
 streamlit.text('Hey, more text.')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
